709a3b83dc560732bd2282b47a88deecb1d4a406
Cintia Yamamoto <cintiafumi@gmail.com>
2021-06-03 00:03:44 -0300

    ✨  feat: create or update user

--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,2 +1,9 @@
 defmodule Flightex do
+  alias Flightex.Users.{CreateOrUpdate, UserAgent}
+
+  def start_agents do
+    UserAgent.start_link(%{})
+  end
+
+  defdelegate create_or_update(params), to: CreateOrUpdate, as: :call
 end
