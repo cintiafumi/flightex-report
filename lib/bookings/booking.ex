08f9170bcf3618a6b77ebef303264c61b04a796f
Cintia Yamamoto <cintiafumi@gmail.com>
2021-06-02 23:35:46 -0300

    ✨  feat: build booking

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -1,9 +1,23 @@
 defmodule Flightex.Bookings.Booking do
+  alias Flightex.Users.User
+
   @keys [:complete_date, :local_origin, :local_destination, :user_id, :id]
+
   @enforce_keys @keys
+
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(complete_date, local_origin, local_destination, %User{id: user_id}) do
+    {:ok,
+     %__MODULE__{
+       user_id: user_id,
+       complete_date: complete_date,
+       local_origin: local_origin,
+       local_destination: local_destination,
+       id: UUID.uuid4()
+     }}
   end
+
+  def build(_complete_date, _local_origin, _local_destination, _user_id),
+    do: {:error, "Invalid parameters."}
 end
